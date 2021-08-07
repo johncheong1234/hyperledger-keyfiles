eba0079ca6619778e855a8e43396eda9fa83143d
johncheong1234 <66426652+johncheong1234@users.noreply.github.com>
2021-08-07 17:49:09 +0800

    Add files via upload

---
+++ com.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+os.system("./network.sh up createChannel -ca")
+os.system("./network.sh deployCC -ccn sgx -ccp ../reconcile/chaincode-javascript-sgx/ -ccl javascript")
+os.system("./network.sh deployCC -ccn primo -ccp ../reconcile/chaincode-javascript-primo/ -ccl javascript")
+os.system("./network.sh deployCC -ccn reconcile -ccp ../reconcile/chaincode-javascript-reconcile/ -ccl javascript")
